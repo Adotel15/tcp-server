5ba1175c1edac0ba23e9a5bbc17f71067326124b
Adrian Dotel <adotel15@hotmail.com>
2024-06-12 22:30:45 +0200

    Optimized server class to have all logic

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -1,4 +1,7 @@
 import http from 'http';
+import fs from 'fs/promises';
+
+import { logNewConnection } from '../utils/log/newConnection.js';
 
 class Server {
     #http_server: any;
@@ -11,14 +14,41 @@ class Server {
     }
 
     createServer = (
-        callback: (
+        callback?: (
             req: http.IncomingMessage,
             res: http.ServerResponse<http.IncomingMessage> & {
                 req: http.IncomingMessage;
             }
         ) => void
     ) => {
-        this.#http_server = http.createServer(callback);
+        this.#http_server = http.createServer(async (req, res) => {
+            const host = req.socket.remoteAddress;
+            const port = req.socket.remotePort;
+            const destinationPath = req.url;
+
+            const requestMetadata = `${new Date().toUTCString()} - to ${destinationPath} from ${host}: ${port}\n`;
+
+            logNewConnection(requestMetadata);
+
+            switch (destinationPath) {
+                case '/':
+                    const html = await fs.readFile(
+                        './public/index.html',
+                        'utf8'
+                    );
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'text/html');
+                    res.end(html);
+                    break;
+                default:
+                    res.statusCode = 404;
+                    res.end('Route not found');
+                    break;
+            }
+
+            res.end('');
+            callback && callback(req, res);
+        });
     };
 
     serverListen = (port: string, callback: () => void) => {
